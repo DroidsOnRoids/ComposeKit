a49e3306d44cc28280be3be522c2b9a896ebe09e
Artur Kosta <artur.kosta@droidsonroids.pl>
2023-09-06 07:28:52 +0200

    Change target sdk to 33

--- buildSrc/src/main/kotlin/config/BuildConfig.kt
+++ buildSrc/src/main/kotlin/config/BuildConfig.kt
@@ -4,7 +4,6 @@ import org.gradle.api.JavaVersion
 
 object BuildConfig {
 
-    const val TARGET_SDK = 32
     val JAVA_VERSION = JavaVersion.VERSION_17
     val JAVA_VERSION_NAME = JAVA_VERSION.toString()
 }
