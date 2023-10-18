f1d37d8dbd442ea4b0dead6f69de2ee1e893cfdf
pawelwojciechowski <pawel.wojciechowski@droidsonroids.pl>
2023-10-18 17:36:59 +0200

    cr improvements

--- composables/draganddroplist/build.gradle.kts
+++ composables/draganddroplist/build.gradle.kts
@@ -3,21 +3,13 @@ plugins {
     id(deps.plugins.playgroundAndroid.get().pluginId)
     id(deps.plugins.playgroundKotlin.get().pluginId)
     id(deps.plugins.playgroundJava.get().pluginId)
+    id(deps.plugins.playgroundCompose.get().pluginId)
 }
 
 android {
     namespace = "com.dor.compose.playground.composables.draganddroplist"
-
-    buildFeatures.compose = true
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = deps.versions.androidXComposeCompiler.get()
-    }
 }
 
 dependencies {
-    implementation(deps.androidX.compose.runtime)
-    implementation(deps.androidX.compose.ui)
-    implementation(deps.androidX.compose.material)
-    implementation(deps.androidX.compose.runtime)
+    implementation(project(":composables:utils"))
 }
