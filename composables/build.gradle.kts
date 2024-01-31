a69dd42b3e32d0ca3685d41d476ed928e304982e
pawgli <pawel.gliniecki@droidsonroids.pl>
2024-01-31 17:48:02 +0100

    set up maven artifact containing all the composables

---
+++ composables/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    with(libs.plugins.composekit) {
+        alias(android.library)
+        alias(publishing)
+    }
+}
+
+android {
+    namespace = "pl.droidsonroids.composekit.composables"
+}
+
+dependencies {
+    api(projects.composables.autoresizedtext)
+    api(projects.composables.collapsingtoolbar)
+    api(projects.composables.draganddroplist)
+    api(projects.composables.foldabletoolbar)
+    api(projects.composables.pulltorefresh)
+}
