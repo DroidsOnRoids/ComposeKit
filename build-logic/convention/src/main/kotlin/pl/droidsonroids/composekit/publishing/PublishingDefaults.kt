e548d4a63ed58d981281aec01dc8f643e1bc3a57
pawgli <pawel.gliniecki@droidsonroids.pl>
2024-01-31 16:57:59 +0100

    update project name in publishing setup

--- build-logic/convention/src/main/kotlin/pl/droidsonroids/composekit/publishing/PublishingDefaults.kt
+++ build-logic/convention/src/main/kotlin/pl/droidsonroids/composekit/publishing/PublishingDefaults.kt
@@ -1,16 +1,16 @@
 package pl.droidsonroids.composekit.publishing
 
 internal object PublishingDefaults {
-    const val Name = "ComposePlayground"
+    const val Name = "ComposeKit"
     const val Year = "2024"
-    const val Url = "https://github.com/DroidsOnRoids/ComposePlayground"
-    const val ScmUrl = "https://github.com/DroidsOnRoids/ComposePlayground"
-    const val ScmConnection = "git@github.com:DroidsOnRoids/ComposePlayground.git"
-    const val ScmDevConnection = "git@github.com:DroidsOnRoids/ComposePlayground.git"
+    const val Url = "https://github.com/DroidsOnRoids/ComposeKit"
+    const val ScmUrl = "https://github.com/DroidsOnRoids/ComposeKit"
+    const val ScmConnection = "git@github.com:DroidsOnRoids/ComposeKit.git"
+    const val ScmDevConnection = "git@github.com:DroidsOnRoids/ComposeKit.git"
     const val LicenseName = "MIT License"
-    const val LicenseUrl = "https://github.com/DroidsOnRoids/ComposePlayground/blob/master/LICENSE"
-    const val BaseGroup = "pl.droidsonroids.composeplayground"
-    const val Version = "0.0.1"
+    const val LicenseUrl = "https://github.com/DroidsOnRoids/ComposeKit/blob/master/LICENSE"
+    const val BaseGroup = "pl.droidsonroids.composekit"
+    const val Version = "0.1.0"
     const val ArtifactIdPropertyName = "POM_ARTIFACT_ID"
     const val DescriptionPropertyName = "POM_DESCRIPTION"
 }
